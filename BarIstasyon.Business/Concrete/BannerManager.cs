4d6ebea8deac79853ff00b9a2b3c0e95e425e07f
Burak Dansık <burakdansik@192.168.1.124>
2025-04-27 16:56:13 +0300

    Service tamamlandı

---
+++ BarIstasyon.Business/Concrete/BannerManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using BarIstasyon.Business.Abstract;
+using BarIstasyon.Entity.Entities;
+using MongoDB.Bson;
+
+namespace BarIstasyon.Business.Concrete
+{
+    public class BannerManager : IBannerService
+    {
+        private readonly IBannerDal
+        public Task<int> TCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TCreateAsync(Banner entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TDeleteAsync(ObjectId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Banner> TGetByIdAsync(ObjectId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Banner>> TGetFilteredListAsync(Expression<Func<Banner, bool>> Predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Banner>> TGetListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TUpdateAsync(Banner entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
--- BarIstasyon.Entity/obj/Debug/net6.0/BarIstasyon.Entity.AssemblyInfo.cs
+++ BarIstasyon.Entity/obj/Debug/net6.0/BarIstasyon.Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarIstasyon.Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05befb353821489d903f769807cca34f3dc1fadd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d72790ce52ee1af7b11d304206ace169daf43a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarIstasyon.Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarIstasyon.Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
