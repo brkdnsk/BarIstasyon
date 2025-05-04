0665190ac3c83a3ba84dfe32b5e7c49ea6479c89
Burak Dansık <bburakdnsk@gmail.com>
2025-05-05 02:44:12 +0300

    Category Results

---
+++ BarIstasyon.Business/Features/CQRS/Results/CategoryResults/GetCategoryByIdQueryResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace BarIstasyon.Business.Features.CQRS.Results.CategoryResults
+{
+	public class GetCategoryByIdQueryResults
+	{
+        public ObjectId CategoryID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
+
---
+++ BarIstasyon.Business/Features/CQRS/Results/CategoryResults/GetCategoryQueryResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace BarIstasyon.Business.Features.CQRS.Results.CategoryResults
+{
+	public class GetCategoryQueryResults
+	{
+        public ObjectId CategoryID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
+
