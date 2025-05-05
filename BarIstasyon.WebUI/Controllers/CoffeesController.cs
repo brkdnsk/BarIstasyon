f3b3e771c22b605baa3ac5b583ab91c498df66f7
Burak Dansık <bburakdnsk@gmail.com>
2025-05-05 03:17:20 +0300

    Coffee Controllers

---
+++ BarIstasyon.WebUI/Controllers/CoffeesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BarIstasyon.Business.Features.CQRS.Commands.CoffeeCommands;
+using BarIstasyon.Business.Features.CQRS.Handlers.CoffeeHandlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarIstasyon.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeesController : ControllerBase  // ControllerBase, API controller'ları için daha uygun bir sınıftır.
+    {
+        private readonly CreateCoffeeCommandHandler _createCoffeeCommandHandler;
+
+        public CoffeesController(CreateCoffeeCommandHandler createCoffeeCommandHandler)
+        {
+            _createCoffeeCommandHandler = createCoffeeCommandHandler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCoffee([FromBody] CreateCoffeeCommand command)  // [FromBody] parametresi ekledim
+        {
+            if (command == null)
+            {
+                return BadRequest("Geçersiz veri.");
+            }
+
+            try
+            {
+                await _createCoffeeCommandHandler.Handle(command);
+                return Ok("Hakkımda Bilgisi Eklendi");
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda uygun yanıt döndürülmesi sağlanır
+                return StatusCode(500, $"Sunucu hatası: {ex.Message}");
+            }
+        }
+    }
+}
--- BarIstasyon.WebUI/obj/Debug/net8.0/BarIstasyon.WebApi.AssemblyInfo.cs
+++ BarIstasyon.WebUI/obj/Debug/net8.0/BarIstasyon.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarIstasyon.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1df6bb5cade7b57756d0b8df87a7f305637651")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7159ed6b22bde394f1496cf99bffdb291b325b97")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarIstasyon.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarIstasyon.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
