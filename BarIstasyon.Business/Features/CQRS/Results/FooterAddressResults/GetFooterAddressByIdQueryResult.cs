7b4c70679dfb61d19e44b4dc75003f52e97247ec
Burak Dansık <bburakdnsk@gmail.com>
2025-05-07 17:30:10 +0300

    Footer Address Controller

---
+++ BarIstasyon.Business/Features/CQRS/Results/FooterAddressResults/GetFooterAddressByIdQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace BarIstasyon.Business.Features.CQRS.Results.FooterAddressResults
+{
+	public class GetFooterAddressByIdQueryResult
+	{
+        public ObjectId FooterAddressId { get; set; }
+
+        public string Description { get; set; }
+
+        public string Adress { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+}
+
---
+++ BarIstasyon.WebUI/Controllers/FooterAddressController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BarIstasyon.Business.Features.CQRS.Commands.FooterAddressCommands;
+using BarIstasyon.Business.Features.CQRS.Handlers.FooterAddressHandlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarIstasyon.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooterAddressController : ControllerBase  // ControllerBase, API controller'ları için daha uygun bir sınıftır.
+    {
+        private readonly CreateFooterAddressCommandHandler _createFooterAddressCommandHandler;
+
+        public FooterAddressController(CreateFooterAddressCommandHandler createFooterAddressCommandHandler)
+        {
+            _createFooterAddressCommandHandler = createFooterAddressCommandHandler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddress([FromBody] CreateFooterAddressCommand command)  // [FromBody] parametresi ekledim
+        {
+            if (command == null)
+            {
+                return BadRequest("Geçersiz veri.");
+            }
+
+            try
+            {
+                await _createFooterAddressCommandHandler.Handle(command);
+                return Ok("Alt Bilgi Eklendi");
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda uygun yanıt döndürülmesi sağlanır
+                return StatusCode(500, $"Sunucu hatası: {ex.Message}");
+            }
+        }
+    }
+}
