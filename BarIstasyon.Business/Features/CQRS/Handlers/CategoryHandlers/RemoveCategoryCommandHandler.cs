df08c99279bc964f7141677aadaff6d6d7052d0f
Burak Dansık <bburakdnsk@gmail.com>
2025-05-10 16:25:06 +0300

    Category Handler completed

---
+++ BarIstasyon.Business/Features/CQRS/Handlers/CategoryHandlers/RemoveCategoryCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using BarIstasyon.Business.Features.CQRS.Commands.CategoryCommands;
+
+using BarIstasyon.DataAccess.Repositories2;
+using BarIstasyon.Entity.Entities;
+
+namespace BarIstasyon.Business.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class RemoveCategoryCommandHandler
+    {
+        private readonly IRepository<Category> _repository;
+        public RemoveCategoryCommandHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveCategoryCommand command)
+        {
+            var category = await _repository.GetByIdAsync(command.id);
+            if (category == null)
+            {
+                throw new Exception("Hakkımda bilgisi bulunamadı, silme işlemi yapılmadı.");
+            }
+
+            // Silme işlemi
+            await _repository.RemoveAsync(command.id);
+
+        }
+    }
+}
+
---
+++ BarIstasyon.Business/Features/CQRS/Handlers/CategoryHandlers/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using BarIstasyon.Business.Features.CQRS.Commands.CategoryCommands;
+
+using BarIstasyon.DataAccess.Repositories2;
+using BarIstasyon.Entity.Entities;
+using MongoDB.Bson;
+using System;
+using System.Threading.Tasks;
+
+public class UpdateCategoryCommandHandler
+{
+    private readonly IRepository<Category> _categoryRepository;
+
+    public UpdateCategoryCommandHandler(IRepository<Category> categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task Handle(UpdateCategoryCommand command)
+    {
+        var category = await _categoryRepository.GetByIdAsync(command.CategoryID);
+        if (category == null)
+        {
+            throw new Exception("Category entity bulunamadı.");
+        }
+
+        category.Name = command.Name;
+        
+
+
+
+
+        await _categoryRepository.UpdateAsync(command.CategoryID, category);  // Güncellenmiş about nesnesini repository'de güncelliyoruz
+    }
+}
+
+
