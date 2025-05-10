351cddeed2e345691ae9a2eb21d18dc017794763
Burak Dansık <bburakdnsk@gmail.com>
2025-05-10 16:15:40 +0300

    Category Commands copmleted

--- BarIstasyon.Business/Features/CQRS/Commands/CategoryCommands/RemoveCategoryCommand.cs
+++ BarIstasyon.Business/Features/CQRS/Commands/CategoryCommands/RemoveCategoryCommand.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using MongoDB.Bson;
+
 namespace BarIstasyon.Business.Features.CQRS.Commands.CategoryCommands
 {
     public class RemoveCategoryCommand
     {
-        public object CategoryId { get; set; }
-        public RemoveCategoryCommand(int id)
+        public ObjectId id { get; set; }
+
+        public RemoveCategoryCommand(ObjectId id)
         {
-            CategoryId = id;
+            this.id = id;
         }
+
+        // Parametresiz constructor da ekleyin, model binding için gerekli olabilir
+        public RemoveCategoryCommand() { }
 
     }
 }
