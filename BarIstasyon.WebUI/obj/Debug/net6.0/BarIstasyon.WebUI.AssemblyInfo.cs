c9e6e315ebd4b0e3131d6e0833480698dcfa1b15
Burak Dansık <bburakdnsk@gmail.com>
2025-05-12 17:34:40 +0300

    UIlayout

--- BarIstasyon.WebUI/obj/Debug/net6.0/BarIstasyon.WebUI.AssemblyInfo.cs
+++ BarIstasyon.WebUI/obj/Debug/net6.0/BarIstasyon.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarIstasyon.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ec0436a858d98a99ae4166cb570e824dba3b68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639f19a97265850c028fe43b6c8da2aad72e6721")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarIstasyon.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarIstasyon.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
