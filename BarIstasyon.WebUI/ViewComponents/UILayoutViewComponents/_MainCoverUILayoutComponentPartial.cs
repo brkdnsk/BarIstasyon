af4874209e08602c8f5aa0f2276696cc7f950b0c
Burak Dansık <bburakdnsk@gmail.com>
2025-05-12 19:45:26 +0300

    Navbar

---
+++ BarIstasyon.WebUI/ViewComponents/UILayoutViewComponents/_MainCoverUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarIstasyon.WebUI.ViewComponents.UILayoutViewComponents
+{
+	public class _MainCoverUILayoutComponentPartial : ViewComponent
+	{
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
--- BarIstasyon.WebUI/obj/Debug/net6.0/BarIstasyon.WebUI.AssemblyInfo.cs
+++ BarIstasyon.WebUI/obj/Debug/net6.0/BarIstasyon.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarIstasyon.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b0a600e293940bf6ef0523ed872f0f9fa68be8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4575958827724bc0d5604094d400641f7a2aed08")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarIstasyon.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarIstasyon.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
