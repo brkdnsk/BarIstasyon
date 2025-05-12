a2c7e8b5ab6ff708d21bc8c55a456c7ebabe8c0a
Burak Dansık <bburakdnsk@gmail.com>
2025-05-12 20:19:52 +0300

    About UILayout

---
+++ BarIstasyon.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarIstasyon.WebUI.ViewComponents.AboutViewComponents
+{
+	public class _AboutUsComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
+
