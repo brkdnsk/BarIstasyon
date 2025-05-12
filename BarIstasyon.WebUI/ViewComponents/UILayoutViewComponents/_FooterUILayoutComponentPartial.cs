769a0e1dc929c3bcaa738879aa89d27d7487c2eb
Burak Dansık <bburakdnsk@gmail.com>
2025-05-12 20:03:53 +0300

    Footer and Main Cover UILayout added

---
+++ BarIstasyon.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarIstasyon.WebUI.ViewComponents.UILayoutViewComponents
+{
+	public class _FooterUILayoutComponentPartial : ViewComponent
+	{
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ BarIstasyon.WebUI/ViewComponents/UILayoutViewComponents/_ScriptUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarIstasyon.WebUI.ViewComponents.UILayoutViewComponents
+{
+	public class _ScriptUILayoutComponentPartial : ViewComponent
+	{
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
